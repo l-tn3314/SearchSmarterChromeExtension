851262f33ad5b4502ca225b82a7d1bb5db75a2d0
Tina Lee <l.tn3314@gmail.com>
2017-03-25 19:29:04 -0400

    updates search suggestion with relevant text

--- eventPage.js
+++ eventPage.js
@@ -1,8 +1,10 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if(request.action == "show") {
-	chrome.tabs.query({ active:true, currentWindow: true}, function (tabs) {
-		 chrome.pageAction.show(tabs[0].id);
-		});
-	}
+  if(request.action == "show") {
+    console.log("show");
+    chrome.tabs.query({ active:true, currentWindow: true}, function (tabs) {
+      chrome.pageAction.show(tabs[0].id);
+    });
+  } 
+    
 });
 
